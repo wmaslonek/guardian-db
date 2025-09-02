67258d141ec7e990c4b082b9ef7c088f0f16b09d
wmaslonek <williamoneline@icloud.com>
2025-09-02 10:48:02 -0300

    refactor: rename ipfs_log module

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,12 @@ pub mod base_guardian;
 pub mod cache;
 pub mod data_store;
 pub mod db_manifest;
-pub mod eqlabs_ipfs_log;
 pub mod error;
 pub mod events;
 pub mod guardian;
 pub mod iface;
 pub mod ipfs_core_api; // New modular IPFS Core API
+pub mod ipfs_log;
 pub mod keystore;
 pub mod message_marshaler;
 pub mod pubsub;
